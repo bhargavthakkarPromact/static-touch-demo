e336d14fe0cef31a33d5fc1eb3d4e8c1721cf2fa
Bhargav Thakkar <bhargavthakkar@promactinfo.com>
2020-03-27 12:22:07 +0530

    changes for 3d touch testing.

---
+++ testingtouching/testingtouching/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testingtouching
+//
+//  Created by Bhargav Thakkar on 27/03/20.
+//  Copyright © 2020 Bhargav Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
